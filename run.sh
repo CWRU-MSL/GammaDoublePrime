fa392fc2266b85c537d95dd8ef03a9a46795696c
Arafath Nihar <axn392@case.edu>
2020-06-16 23:31:30 -0400

    run file added

---
+++ run.sh
@@ -0,0 +1,7 @@
+python3 seedpointextraction.py
+python3 contourestimation.py
+python3 Ellipsefitting.py
+python3 Calculate_area.py
+python3 Final_gamma_double.py > FILE
+python3 Final_gamma_prime.py
+python3 Final_results.py > file
